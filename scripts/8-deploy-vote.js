00b5e08034eb0174a1683a821e9ca3e05e52dba6
pr2a <31780665+pr2a@users.noreply.github.com>
2022-03-20 18:46:19 -0400

    Update 8-deploy-vote.js

---
+++ scripts/8-deploy-vote.js
@@ -0,0 +1,43 @@
+import sdk from "./1-initialize-sdk.js";
+
+// Grab the app module address.
+const appModule = sdk.getAppModule(
+  "0x2DEAda8c1CEFa7f3BD3a9Ce7BaCF974bB958a06C",
+);
+
+(async () => {
+  try {
+    const voteModule = await appModule.deployVoteModule({
+      // Give your governance contract a name.
+      name: "TestDAO's Epic Proposals",
+
+      // This is the location of our governance token, our ERC-20 contract!
+      votingTokenAddress: "0x5A2A3bB8DA5a4938FE172038FEe0133215612d8A",
+
+      // After a proposal is created, when can members start voting?
+      // For now, we set this to immediately.
+      proposalStartWaitTimeInSeconds: 0,
+
+      // How long do members have to vote on a proposal when it's created?
+      // Here, we set it to 24 hours (86400 seconds)
+      proposalVotingTimeInSeconds: 24 * 60 * 60,
+
+      // Will explain more below.
+      votingQuorumFraction: 0,
+
+      // What's the minimum # of tokens a user needs to be allowed to create a proposal?
+      // I set it to 0. Meaning no tokens are required for a user to be allowed to
+      // create a proposal.
+      minimumNumberOfTokensNeededToPropose: "0",
+    });
+
+    console.log(
+      "✅ Successfully deployed vote module, address:",
+      voteModule.address,
+    );
+  } catch (err) {
+    console.error("Failed to deploy vote module", err);
+  }
+})();
+
+//vote mod addr: 0x8aA0491aed30e9cA2DFe995BdED882197166ec42

4988598afd10025e72d1c0fd3a309fe2633e7240
pr2a <31780665+pr2a@users.noreply.github.com>
2022-03-20 18:48:32 -0400

    Update 4-set-claim-condition.js

---
+++ scripts/4-set-claim-condition.js
@@ -0,0 +1,23 @@
+import sdk from "./1-initialize-sdk.js";
+
+const bundleDrop = sdk.getBundleDropModule(
+  "0x4841676a7a2CaB6816284CA1e950c617773C5479",
+);
+
+(async () => {
+  try {
+    const claimConditionFactory = bundleDrop.getClaimConditionFactory();
+    // Specify conditions.
+    claimConditionFactory.newClaimPhase({
+      startTime: new Date(),
+      maxQuantity: 50_000,
+      maxQuantityPerTransaction: 1,
+    });
+    
+    
+    await bundleDrop.setClaimCondition(0, claimConditionFactory);
+    console.log("✅ Successfully set claim condition on bundle drop:", bundleDrop.address);
+  } catch (error) {
+    console.error("Failed to set claim condition", error);
+  }
+})()
